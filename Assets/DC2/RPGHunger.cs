476ad0a2a63611d60895e334df0b3dae0657909e
Martin Labuť <martin.labut@volny.cz>
2017-11-06 23:03:39 +0100

    small changes

---
+++ Assets/DC2/RPGHunger.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RPGHunger : GoapSensor {
+
+    public float saturationMultiplier = 1;
+    public float saturationLevel = 0;
+
+    public override void UpdateSensor() {
+        GetMemory().GetWorldState().Set( "saturation", saturationLevel );
+    }
+
+    public void EatItem()
+
+}
